4f9a5892d31f4c6849bad820bb0050cdb090a18b
OscarPereyra1992 <oscarpereyra.mhs@gmail.com>
2023-05-29 08:40:07 -0300

    base uptaded

--- bank_modern_app/vite.config.js
+++ bank_modern_app/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/OscarPereyra1992/React-Tailwind-Ui-Project'
 })
